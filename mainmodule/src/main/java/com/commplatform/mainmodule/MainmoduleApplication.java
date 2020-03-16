c870857de2282eebee699aad6d7100b2ce85f6a2
lyx <924847972@qq.com>
2020-03-16 18:58:58 +0800

    创建主模块

---
+++ mainmodule/src/main/java/com/commplatform/mainmodule/MainmoduleApplication.java
@@ -0,0 +1,13 @@
+package com.commplatform.mainmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainmoduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainmoduleApplication.class, args);
+    }
+
+}
